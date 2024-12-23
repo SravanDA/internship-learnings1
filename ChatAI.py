466107330b4cd89cc6402af6bae326921217a4cf
Analytics With Sravan <164868347+SravanDA@users.noreply.github.com>
2024-12-24 01:04:48 +0530

    ChatAI.py
    (Speech to text to text to speech)

--- ChatAI.py
+++ ChatAI.py
@@ -1,6 +1,12 @@
+#speach to text to text to speach
+
 import pyttsx3
 import speech_recognition as sr
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv 
+
+load_dotenv()
 
 class VoiceAssistant:
     def __init__(self, api_key):
@@ -39,6 +45,10 @@ class VoiceAssistant:
                 self.speak(response)
 
 if __name__ == "__main__":
-    API_KEY = "AIzaSyC9SOZ_dr2Dfg53HNxrAWrurhwKORT_DjY"  
-    assistant = VoiceAssistant(API_KEY)
-    assistant.run()
+    API_KEY = os.getenv("GOOGLE_API_KEY")
+    
+    if not API_KEY:
+        print("Error: GOOGLE_API_KEY is not set in the environment variables.")
+    else:
+        assistant = VoiceAssistant(API_KEY)
+        assistant.run()
